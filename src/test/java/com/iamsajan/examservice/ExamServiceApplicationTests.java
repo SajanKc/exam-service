f27a2134714d216fbd948af78f66325d96936863
Sajan Kc <sajan1996kc@gmail.com>
2022-05-12 08:31:06 +0545

    initial commit

---
+++ src/test/java/com/iamsajan/examservice/ExamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamsajan.examservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
