8d47137cadca671d4ecd7db11b79ff611aeb2825
Sajan Kc <sajan1996kc@gmail.com>
2022-05-15 07:33:14 +0545

    spring security implemented using jwt

---
+++ src/main/java/com/iamsajan/examservice/controller/AuthenticateController.java
@@ -0,0 +1,53 @@
+package com.iamsajan.examservice.controller;
+
+import com.iamsajan.examservice.config.JWtUtils;
+import com.iamsajan.examservice.model.JwtRequest;
+import com.iamsajan.examservice.model.JwtResponse;
+import com.iamsajan.examservice.service.impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticateController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Autowired
+    private JWtUtils jWtUtils;
+
+    @PostMapping("/generate-token")
+    public ResponseEntity<?> generateToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+        try {
+            authenticate(jwtRequest.getUsername(), jwtRequest.getPassword());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception("User not found");
+        }
+        UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(jwtRequest.getUsername());
+        String token = jWtUtils.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("User Disabled: " + e.getMessage());
+        } catch (BadCredentialsException e) {
+            throw new Exception("Invalid Credentials: " + e.getMessage());
+        }
+    }
+}
--- src/main/java/com/iamsajan/examservice/model/User.java
+++ src/main/java/com/iamsajan/examservice/model/User.java
@@ -2,15 +2,18 @@ package com.iamsajan.examservice.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,4 +30,30 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JsonIgnore
     private Set<UserRole> userRoles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Authority> set = new HashSet<>();
+
+        this.userRoles.forEach(userRole -> {
+            set.add(new Authority(userRole.getRole().getRoleName()));
+        });
+
+        return set;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 }
