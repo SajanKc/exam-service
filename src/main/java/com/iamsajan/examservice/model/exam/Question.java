6f95ae15c2ee6b4c657ec5c7aa7b64fb1b6a39e9
Sajan Kc <sajan1996kc@gmail.com>
2022-08-16 12:08:23 +0545

    answer field hidden for client side

--- src/main/java/com/iamsajan/examservice/model/exam/Question.java
+++ src/main/java/com/iamsajan/examservice/model/exam/Question.java
@@ -1,5 +1,6 @@
 package com.iamsajan.examservice.model.exam;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.hibernate.annotations.Cascade;
 
@@ -23,6 +24,7 @@ public class Question {
     private String option2;
     private String option3;
     private String option4;
+    @JsonIgnore
     private String answer;
     @Transient
     private String givenAnswer;
